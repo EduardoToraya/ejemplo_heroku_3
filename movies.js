9d34519e8c729d45ac091ef83a21bceae681e77d
EduardoToraya <eduar_ts1997@hotmail.com>
2019-10-14 17:43:53 -0500

    updatacredentials

--- movies.js
+++ movies.js
@@ -1,9 +1,17 @@
-const credentials = require("./credentials.js")
-
 const request = require("request")
 
+
+if(process.env.Node_ENV === "production"){
+  var apikey = process.env.API_KEY
+}
+else{
+  const credentials = require("./credentials.js")
+  var apikey = credentials.apikey
+}
+
+
 const omdbMovie = function(title, callback){
-  const url = "http://www.omdbapi.com/?apikey=" + credentials.apikey + "&t=" + title
+  const url = "http://www.omdbapi.com/?apikey=" + apikey + "&t=" + title
 
   request({url, json: true}, function(error, response){
     //console.log(response.body)
@@ -33,7 +41,7 @@ const omdbMovie = function(title, callback){
 
 
 const omdbSeason = function(title, season, callback){
-  const url = "http://www.omdbapi.com/?apikey=" + credentials.apikey + '&t='
+  const url = "http://www.omdbapi.com/?apikey=" + apikey + '&t='
     + title + "&Season=" + season
 
     request({url, json: true}, function(error,response){
